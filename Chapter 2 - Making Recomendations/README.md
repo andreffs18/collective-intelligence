dbd5bf3bf3567983eb7b0455ff81fe98866387d7
andreffs18 <andreffs18@gmail.com>
2018-01-07 16:11:43 +0000

    Add missing dataset from chapter 2

---
+++ Chapter 2 - Making Recomendations/README.md
@@ -0,0 +1,2 @@
+# dataset used can be fond here
+[http://grouplens.org/datasets/movielens/](http://grouplens.org/datasets/movielens/)
---
+++ Chapter 2 - Making Recomendations/dataset.py
@@ -0,0 +1,37 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Created by andresilva on 9/20/16"""
+import os
+DATASET_PATH = os.getcwd() + '/dataset'
+
+
+def get_movie_dict():
+    # build movie dictitonary
+    movie_dict = {}
+    with open(DATASET_PATH + "/u.item", "r") as uitem:
+        for row in uitem.readlines():
+            data = row.split("|")
+            movie_id, movie_title, imdb_url = data[0], data[1], data[4]
+            movie_dict[movie_id] = {
+                'title': movie_title,
+                'imdb': imdb_url
+            }
+    return movie_dict
+
+
+def get_dataset():
+    """Merge data from two different files and build critics dictionary"""
+    # build dataset to use
+    dataset = dict()
+    with open(DATASET_PATH + "/u.data", "r") as udata:
+        for row in udata.readlines():
+            u_id, i_id, rating, _ = row.split("\t")
+            if u_id not in dataset:
+                dataset[u_id] = {}
+            dataset[u_id][i_id] = float(rating)
+
+    return dataset
+
+
+__author__ = "andresilva"
+__email__ = "andre@unbabel.com"
